6e8de8c27cbb8e3cb97bb8f09386d0246b1a8de8
Muzammil <muzamilrafiq000@gmail.com>
2024-08-14 17:53:31 +0500

    working with event handlers and middlewares

--- server2.js
+++ server2.js
@@ -4,42 +4,76 @@ const PORT = process.env.PORT;
 const users = [
   {
     id: 1,
-    name: 'muzammil',
+    name: "muzammil",
   },
   {
     id: 2,
-    name: 'arshad',
+    name: "arshad",
   },
   {
     id: 3,
-    name: 'loki',
+    name: "loki",
   },
 ];
+//logger middleware
+const loggerMiddleWare = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
 
-const server = createServer((req, res) => {
-  if (req.url === "/api/users" && req.method === "GET") {
-    res.setHeader("Content-Type", "application/json");
+const jsonMiddleWare = (req, res, next) => {
+  res.setHeader("Content-Type", "application/json");
+  next();
+};
+
+// Route Handler
+
+const getUsersHandler = (req, res) => {
     res.write(JSON.stringify(users));
     res.end();
+};
+
+const getUserByIdHandler = (req, res) => {
+  const id = req.url.split("/")[3];
+  const user = users.find((user) => user.id === parseInt(id));
+  if (user) {
+    res.write(JSON.stringify(user));
+  } else {
+    res.statusCode = 404;
+    res.write(
+      JSON.stringify({
+        messsage: "User Not Found",
+      })
+    );
   }
-    else if (req.url.match(/\/api\/users\/([0-9]+)/) && req.method === 'GET'){
-        const id = req.url.split('/')[3];
-        console.log("id:", id);
-        debugger;
-        res.setHeader("Content-Type", "application/json");
-        res.write(JSON.stringify({
-            id: 3,
-            name: 'Muzammil'
-        }));
-        res.end();
-    
-  }else{
-    res.setHeader("Content-Type", "application/json");
-    res.write(JSON.stringify({
-        messsage: "Route Not Found"
-    }));
-    res.end();
-  }
+  res.end();
+};
+
+const notFoundHandler = (req, res) => {
+  res.statusCode = 404;
+  res.write(
+    JSON.stringify({
+      messsage: "Route Not Found",
+    })
+  );
+  res.end();
+};
+
+const server = createServer((req, res) => {
+  loggerMiddleWare(req, res, () => {
+    jsonMiddleWare(req, res, () => {
+      if (req.url === "/api/users" && req.method === "GET") {
+        getUsersHandler(req, res);
+      } else if (
+        req.url.match(/\/api\/users\/([0-9]+)/) &&
+        req.method === "GET"
+      ) {
+        getUserByIdHandler(req, res);
+      }else {
+      notFoundHandler(req, res);
+      }
+    });
+  });
 });
 
 server.listen(PORT, () => {
