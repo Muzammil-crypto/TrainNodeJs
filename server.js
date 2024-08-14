9f590ab1612e2f7a29165d2a82018f0b1522af4d
Muzammil <muzamilrafiq000@gmail.com>
2024-08-14 15:11:58 +0500

    working on returning files instead of plain text

--- server.js
+++ server.js
@@ -1,17 +1,29 @@
 import http from "http";
-const PORT = 8000;
+const PORT = process.env.PORT;
 
 const server = http.createServer((req, res) => {
-    res.setHeader('Content-Type', 'text/plain');
-    // res.setHeader('Content-Type', 'text/html');
-    // res.statusCode = 400;
-  res.writeHead(500, {'Content-Type': 'application/json'});
-  res.end(JSON.stringify({
-    message: 'Server Error '
-  }));
-
-
-
+  // res.setHeader('Content-Type', 'text/plain');
+  // res.setHeader('Content-Type', 'text/html');
+  // res.statusCode = 400;
+  try {
+    if (req.method === "GET") {
+      if (req.url === "/") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end("<h1> HELLO MY WORLD AT HOME</h1>");
+      } else if (req.url === "/about") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end("<h1> HELLO MY WORLD AT ABOUT</h1>");
+      } else {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        res.end("<h1>PAGE NOT FOUND</h1>");
+      }
+    } else {
+      throw new Error("Method Not Allowed");
+    }
+  } catch (error) {
+    res.writeHead(500, { "Content-Type": "text/plain" });
+    res.end("Server Error");
+  }
 });
 
 server.listen(PORT, () => {
